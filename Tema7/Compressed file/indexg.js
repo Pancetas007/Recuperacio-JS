95f4dd5b37a9862dc053076d10c95517e64a471d
Pancetas007 <73484401+Pancetas007@users.noreply.github.com>
2022-05-02 13:54:37 +0200

    Add files via upload

--- Tema7/Compressed file/indexg.js
+++ Tema7/Compressed file/indexg.js
@@ -5,11 +5,63 @@ function main() {
 }
 
 function cargarTabla() {
-    fetch("https://api.covid19tracking.narrativa.com/api/country/spain/region/c_valenciana?date_from=2020-03-20&date_to=2020-03-22", {
-            method: "GET"            
+    var spinner = document.getElementById("spinner");
+
+
+    fetch("https://api.covid19tracking.narrativa.com/api/country/spain/region/c_valenciana?date_from=2022-03-02&date_to=2022-05-02", {
+            method: "GET"
         })
         .then(response => response.json())
         .then(data => {
+            spinner.style.display = "none";
             console.log(data);
+
+            const fecha = [];
+            const totalInf = [];
+            const totalDef = [];
+
+            for (let i in data.dates) {
+                fecha.push(i);
+                totalInf.push(data.dates[i].countries.Spain.regions[0].today_new_confirmed);
+                totalDef.push(data.dates[i].countries.Spain.regions[0].today_new_deaths)
+            }
+
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: fecha,
+                    datasets: [{
+                        label: ["Defuncions"],
+                        data: totalDef,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)'
+                        ],
+                        borderWidth: 1 
+                    },{
+                        label: ["Infectats"],
+                        data: totalInf,
+                        backgroundColor: [
+                            'rgba(54, 162, 235, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+
+
         })
 }
